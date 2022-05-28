e1adefa4d6e899dc9ebcbcf2b827fc43fd16a734
Yashvardhan Arora <yash22arora@gmail.com>
2022-05-29 02:31:30 +0530

    Fix public files and add footer

--- client-new/src/App.js
+++ client-new/src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Dashboard from './components/Dashboard';
+import Footer from './components/Footer';
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
                     <Dashboard/>
                 </Route>
             </Switch>
+            <Footer/>
         </Router>
     );
     }
---
+++ client-new/src/components/Footer.js
@@ -0,0 +1,12 @@
+import banner from '../assets/ServatomBanner.jpg';
+import '../index.css';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <img src={banner} />
+        </div>
+    )
+}
+
+export default Footer;
